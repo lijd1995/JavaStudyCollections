c3d0c2eab93fc46a60d17ce496dd3c668448afce
xiaoli <lijd1995@163.com>
2020-05-19 23:14:57 +0800

    全排列二

--- Algrithm/递归/Solution2.java
+++ Algrithm/递归/Solution2.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Solution2 {
-    public static List<List<Integer>> permute(int[] nums) {
-        LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
-        res.add(new ArrayList<Integer>());
-        for (int n : nums) {
-            int size = res.size();
-            for (; size > 0; size--) {
-                List<Integer> r = res.pollFirst();
-                for (int i = 0; i <= r.size(); i++) {
-                    List<Integer> t = new ArrayList<Integer>(r);
-                    t.add(i, n);
-                    res.add(t);
-                }
-            }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        recur(nums,result,new ArrayList<Integer>(),visited);
+        return result;
+    }
+
+    private void recur(int[] nums, List<List<Integer>> result, ArrayList<Integer> list, boolean[] visited) {
+        if (list.size() == nums.length){
+            result.add(new ArrayList<>(list));
+            return;
         }
-        return res;
+        for(int i=0;i<nums.length;i++){
+            if (visited[i]) continue;
+            if (i>0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            list.add(nums[i]);
+            visited[i] = true;
+            recur(nums,result,list,visited);
+            visited[i] = false;
+            list.remove(list.size()-1);
+        }
     }
 
     public static void main(String[] args) {
